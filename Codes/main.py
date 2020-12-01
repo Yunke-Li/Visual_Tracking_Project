aa28838edf50e60c345c49396c92eaf786aa8455
LI <liyunke81@gmail.com>
2020-12-01 23:40:04 +0100

    code update

--- Codes/main.py
+++ Codes/main.py
@@ -1,7 +1,8 @@
 import numpy as np
 import os
 import math
-from utils import GetImgNameByEveryDir
+import cv2
+from utils import GetImgNameByEveryDir, get_context
 
 # set path
 path = '..\\data'
@@ -31,3 +32,62 @@ conf = np.exp(-0.5 / alpha * np.sqrt(dist))
 conf = conf/np.sum(conf)
 conf_fft = np.fft.fft2(conf)
 # store pre-computed weight window
+hamming_window = np.hamming(size_x).reshape(size_x, 1) * np.hanning(size_y).reshape(size_y, 1).transpose()
+hamming_window = hamming_window.astype('double')
+sigma = (target_size[0] + target_size[1])/2
+window = hamming_window * np.exp(-0.5/(sigma*sigma)*dist)
+window = hamming_window/hamming_window.sum()
+#
+files = os.listdir(path)   # 读入文件夹
+num_img = len(files)
+for frame in range(1, num_img):
+    sigma = sigma * scale
+    window = hamming_window * np.exp(-0.5 / (sigma * sigma) * dist)
+    window = hamming_window / hamming_window.sum()
+    # load image
+    img = cv2.imread(path + '\\' + files[frame-1])
+    if img[2] > 1:
+        im = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    contextprior = get_context(im, pos, sz, window) #### sz?
+    #
+    if frame > 1:
+        # calculate response of the confidence map at all locations
+        confmap = (np.fft.ifft2(Hstcf * np.fft.fft2(contextprior))).real()
+        #target location is at the maximum response
+        [row, col] = np.unravel_index(np.argmax(confmap), confmap.shape)
+        row = row + 1
+        col = col + 1
+        pos = pos - [size_x/2, size_y/2] + [row, col]
+        contextprior = get_context(im, pos, sz, window) ## sz?
+        maxconf[frame-2] = np.max(conftmp)
+        # update scale by Eq.(15)
+        if frame % (num + 2) == 0:
+            scale_curr = 0
+            for kk in range(1, num):
+                scale_curr = scale_curr + np.sqrt(maxconf[frame-kk-1]/maxconf[frame-kk-2])
+            scale = (1 - lam) * scale + lam * (scale_curr/num)
+
+    # update the spatial context model h^sc in Eq.(9)
+    contextprior = get_context(im, pos, sz, window) # sz??
+    hscf = conf_fft / np.fft.fft2(contextprior + np.finfo(float).eps)
+    # update the spatio-temporal context model by Eq.(12)
+    if frame == 1:
+        Hstcf = hscf
+    else:
+        # update the spatio-temporal context model H^stc by Eq.(12)
+        Hstcf = (1 - rho) * Hstcf + rho * hscf
+
+    # visualization
+    target_size
+
+
+
+
+
+
+
+
+
+
+print()
