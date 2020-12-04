f984b41b65ac0a45a7c1cec70b0970b571e34aff
LI <liyunke81@gmail.com>
2020-12-04 03:23:57 +0100

    code update and debug

--- Codes/main.py
+++ Codes/main.py
@@ -2,24 +2,24 @@ import numpy as np
 import os
 import math
 import cv2
-from utils import GetImgNameByEveryDir, get_context
+from utils import get_context
 
 # set path
 path = '..\\data'
-# FileName, FileNameWithPath, FileDir = GetImgNameByEveryDir(path)
 # initialization
 inistate = [161, 65, 75, 95]
 pos = [inistate[1]+inistate[3]/2, inistate[0]+inistate[2]/2]
 target_size = [inistate[3], inistate[2]]
+target_size = np.array(target_size)
 # parameters according to the paper
 padding = 1
-rho = 0.075
+rho = 0.5
 size_x = math.floor(target_size[0]*(1+padding))
 size_y = math.floor(target_size[1]*(1+padding))
 # parameters of scale update. See Eq.(15)
 scale = 1
 lam = 0.25
-num = 5
+num = 30
 # store pre-computed confidence map
 alpha = 2.25
 x = np.arange(1, size_x+1, 1) - np.floor(size_x/2)
@@ -36,40 +36,53 @@ hamming_window = np.hamming(size_x).reshape(size_x, 1) * np.hanning(size_y).resh
 hamming_window = hamming_window.astype('double')
 sigma = (target_size[0] + target_size[1])/2
 window = hamming_window * np.exp(-0.5/(sigma*sigma)*dist)
-window = hamming_window/hamming_window.sum()
+window = hamming_window/window.sum()
 #
 files = os.listdir(path)   # 读入文件夹
 num_img = len(files)
-for frame in range(1, num_img):
+maxconf = np.empty([num_img-1])
+conftmp = []
+
+for frame in range(1, num_img+1):
     sigma = sigma * scale
     window = hamming_window * np.exp(-0.5 / (sigma * sigma) * dist)
-    window = hamming_window / hamming_window.sum()
+    window = hamming_window / window.sum()
     # load image
     img = cv2.imread(path + '\\' + files[frame-1])
-    if img[2] > 1:
+    if img.shape[2] > 1:
         im = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    contextprior = get_context(im, pos, sz, window) #### sz?
+    sz = [size_x, size_y]
+    contextprior = get_context(im, pos, sz, window)
     #
     if frame > 1:
         # calculate response of the confidence map at all locations
-        confmap = (np.fft.ifft2(Hstcf * np.fft.fft2(contextprior))).real()
+        confmap = np.fft.ifft2(Hstcf * np.fft.fft2(contextprior))
+        confmap = np.real(confmap)
         #target location is at the maximum response
         [row, col] = np.unravel_index(np.argmax(confmap), confmap.shape)
         row = row + 1
         col = col + 1
-        pos = pos - [size_x/2, size_y/2] + [row, col]
-        contextprior = get_context(im, pos, sz, window) ## sz?
+        pos = [pos[0]-size_x/2+row, pos[1]-size_y/2+col]
+        sz = [size_x, size_y]
+        print('frame:', frame)
+        # print('pos:', pos)
+        # print('row:', row)
+        # print('col:', col)
+        contextprior = get_context(im, pos, sz, window)
+        conftmp = np.fft.ifft2(Hstcf * np.fft.fft2(contextprior))
+        conftmp = np.real(conftmp)
         maxconf[frame-2] = np.max(conftmp)
+
         # update scale by Eq.(15)
         if frame % (num + 2) == 0:
             scale_curr = 0
-            for kk in range(1, num):
+            for kk in range(1, num+1):
                 scale_curr = scale_curr + np.sqrt(maxconf[frame-kk-1]/maxconf[frame-kk-2])
             scale = (1 - lam) * scale + lam * (scale_curr/num)
 
     # update the spatial context model h^sc in Eq.(9)
-    contextprior = get_context(im, pos, sz, window) # sz??
+    sz = [size_x, size_y]
+    contextprior = get_context(im, pos, sz, window)
     hscf = conf_fft / np.fft.fft2(contextprior + np.finfo(float).eps)
     # update the spatio-temporal context model by Eq.(12)
     if frame == 1:
@@ -79,15 +92,20 @@ for frame in range(1, num_img):
         Hstcf = (1 - rho) * Hstcf + rho * hscf
 
     # visualization
-    target_size
+    target_size = target_size * scale
+    pos_arr = np.array(pos)
+    target_size_arr = np.array(target_size)
+    rect_ori = np.flip((pos_arr - target_size_arr/2)) # origin of rectangle
+    w = target_size[1]
+    h = target_size[0]
+    p1_x = int(rect_ori[0])
+    p1_y = int(rect_ori[1])
+    p2_x = int(p1_x + w)
+    p2_y = int(p1_y + h)
+    w = target_size[1]
+    h = target_size[0]
+    result = cv2.rectangle(img, (p1_x, p1_y), (p2_x, p2_y), (255, 0, 0), 2)
+    result_resize = cv2.resize(result, None, fx=3, fy=3, interpolation=cv2.INTER_AREA)
+    cv2.imshow('result', result_resize)
+    cv2.waitKey(40)
 
-
-
-
-
-
-
-
-
-
-print()
--- Codes/utils.py
+++ Codes/utils.py
@@ -1,51 +1,44 @@
-import numpy
-import os
-
-
-def GetImgNameByEveryDir(file_dir):
-    FileNameWithPath = []
-    FileName = []
-    FileDir = []
-    videoProperty = ['.jpg']
-    for root, dirs, files in os.walk(file_dir):
-        for file in files:
-            if os.path.splitext(file)[1] in videoProperty:
-                FileNameWithPath.append(os.path.join(root, file))  # path
-                FileName.append(file)  # name
-                FileDir.append(root[len(file_dir):])  # folder
-                # FileNameWithPath.sort(key=lambda x:int(x.split('.')[0]))
-                FileName.sort(key=lambda x:int(x.split('.')[0]))
-                # FileDir.sort(key=lambda x:int(x.split('.')[0]))
-    return FileName, FileNameWithPath, FileDir
-
-
-import math
 import numpy as np
 
 
 def get_context(im, pos, sz, window):
-    x = [pos[1] + i - (sz[1]/2) for i in range(1, sz[1]+1)]
-    y = [pos[0] + j - (sz[0]/2) for j in range(1, sz[0]+1)]
-    xs = [math.floor(i) for i in x]
-    ys = [math.floor(j) for j in y]
+    x = [pos[1] + i - (sz[1] / 2) for i in range(1, sz[1] + 1)]
+    y = [pos[0] + j - (sz[0] / 2) for j in range(1, sz[0] + 1)]
+    xs = np.floor(x)
+    ys = np.floor(y)
 
-    for i in xs:
-        if i < 1:
-            i = 1
+    # for i in xs:
+    #     if i < 1:
+    #         i = 1
+    xs[xs < 1] = 1
 
-    for j in ys:
-        if j < 1:
-            j = 1
+    # for j in ys:
+    #     if j < 1:
+    #         j = 1
+    ys[ys < 1] = 1
 
-    for i in xs:
-        if i > im.shape(1):
-            i = im.shape(1)
+    # for i in xs:
+    #     if i > im.shape[1]:
+    #         i = im.shape[1]
+    xs[xs > im.shape[1]] = im.shape[1]
 
-    for j in ys:
-        if j > im.shape(0):
-            j = im.shape(0)
+    # for j in ys:
+    #     if j > im.shape[0]:
+    #         j = im.shape[0]
+    ys[ys > im.shape[0]] = im.shape[0]
 
-    out = im[ys, xs, :]
+    xs = xs.astype(np.int)
+    ys = ys.astype(np.int)
+    xs_min = xs.min()
+    ys_min = ys.min()
+    szx = sz[0]
+    szy = sz[1]
+    if ys[-1] + 1 - ys[0] == 190:
+        out = im[ys[0] - 1:ys[-1], xs[0] - 1:xs[-1]]
+        print('good')
+    else:
+        out = im[ys_min:(ys_min + szx), xs_min:(xs_min + szy)]
+        print('bad')
     out = out - np.mean(out)
     out = window * out
 
